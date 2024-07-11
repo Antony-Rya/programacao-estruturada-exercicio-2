c0e813db43f884044408736d13c4f67e36ed06b7
Antony-Rya <159479364+Antony-Rya@users.noreply.github.com>
2024-07-11 20:21:13 +0000

    aaa

--- main.py
+++ main.py
@@ -40,10 +40,17 @@ def q3():
     soma = 0
     cont = 0
     nota = 0
-    while nota != -1:
+    while True:
         nota = float(input())
-        soma += nota
-        cont += 1
-    print(soma/cont)
+        if nota == -1:
+            break
+        if nota in range(0, 11):
+            soma += nota
+            cont += 1
+        else:
+            print("Nota invalida")
+            break
+    if cont> 0:
+        print(soma/cont)
 
 q3()
